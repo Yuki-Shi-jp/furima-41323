a2b6a2e3054c515cbd39184b1fd348ac02273370
Yuki-Shi-jp <y.shi0719@gmail.com>
2024-08-01 15:13:31 +0900

    itemモデルへバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,18 @@
 class Item < ApplicationRecord
-  belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  validates :product_name, :product_description, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :category_id, numericality: { other_than: 1 }
+  validates :sales_status_id, numericality: { other_than: 1 }
+  validates :shipping_fee_status_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :scheduled_delivery_id, numericality: { other_than: 1 }
 
+  belongs_to :user
   has_one_attached :image
+  belongs_to_active_hash :category
+  belongs_to_active_hash :sales_status
+  belongs_to_active_hash :shipping_fee_status
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :scheduled_delivery
 end
