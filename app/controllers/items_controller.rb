fda2fa8b445a7bfafed6e23d713b19c531596d5f
Yuki-Shi-jp <y.shi0719@gmail.com>
2024-08-05 18:32:57 +0900

    商品情報編集機能の表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :update]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update]
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -24,7 +26,29 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def correct_user
+    return if current_user.id == @item.user_id
+
+    redirect_to root_path
+  end
 
   def item_params
     params.require(:item).permit(:image, :product_name, :product_description, :price, :category_id, :sales_status_id,
