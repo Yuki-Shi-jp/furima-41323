15d2bfd8ebda86c3b513c7df6d3b31baad4418a9
Yuki-Shi-jp <y.shi0719@gmail.com>
2024-08-07 15:04:04 +0900

    Formオブジェクト作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,13 @@
 class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
   def index
-    @order = Order.new
+    @order_address = OrderAddress.new
   end
 
   def create
-    @order = Order.new(order_params)
-    if @order.valid?
-      @order.save
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
       redirect_to root_path
     else
       render 'index', status: :unprocessable_entity
@@ -21,6 +21,7 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:order).permit(:user, :item)
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number
+    ).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,30 +1,3 @@
 class Order < ApplicationRecord
-  include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number
-
-  validates :user_id, presence: true
-  validates :item_id, presence: true
-  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/ }
-  validates :prefecture_id, presence: true
-  validates :city, presence: true
-  validates :address, presence: true
-  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
-
-  belongs_to :user
-  belongs_to :item
   has_one :address
-
-  def save
-    order = Order.create(user_id:, item_id:)
-
-    Address.create(
-      postal_code:,
-      prefecture_id:,
-      city:,
-      address:,
-      building_name:,
-      phone_number:,
-      order_id:
-    )
-  end
 end
